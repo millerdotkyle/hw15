6537f13871527ca9078afbf7a2d0a184ff73bd18
millerdotkyle <kylenotfound@gmail.com>
2023-10-15 21:01:22 -0400

    added logic for database.js
    -based on PWA-23

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,41 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  //crteate a connection to the database
+  const db = await initdb();
+  //create a transaction
+  const tx = db.transaction('jate', 'readwrite');
+  //open up the object store
+  const store = tx.objectStore('jate');
+  //put the content in the object store
+  await store.put(content);
+  //wait for the transaction to complete
+  await tx.done;
+  //log that the database was updated
+  console.log('jate database updated');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  //create a connection to the database
+  const db = await initdb();
+  //create a transaction
+  const tx = db.transaction('jate', 'readonly');
+  //open up the object store
+  const store = tx.objectStore('jate');
+  //get all the content from the object store
+  const allContent = await store.getAll();
+  //wait for the transaction to complete
+  await tx.done;
+  //Check for content
+  if (allContent.length === 0) {
+    console.log('No content in database');
+    return;
+  }
+
+  //return the content
+  return allContent;
+};
 
 initdb();
